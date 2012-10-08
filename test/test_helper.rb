942ab3c89b372c14c96fe217f0b0ef00e612adbf
Vladimir <nettsundere@gmail.com>
2012-10-08 20:12:59 +0400

    Added tests

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+helper_path = '/../../../test/test_helper'
+
+helper = File.expand_path(File.dirname(__FILE__) + helper_path)
+
+# Rails 2
+if !File.exists?(helper)
+  helper = File.expand_path(File.dirname(__FILE__) + "/.." + helper_path)
+end
+
+require helper
