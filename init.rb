4c4645ae874789b112ce80262e5086d4030f5d30
Jaap de Haan <jaap.dehaan@alpsalpine.com>
2025-05-27 16:23:43 +0200

    fix: Remove direct base_rspec dependency in Gemfile

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_stealth do
   name        'Stealth Plugin'
   author      'Riley Lynch, Restream, Jérôme BATAILLE'
   description 'Enables users to disable Redmine email notifications for their actions'
-  version     '1.0.2'
+  version     '1.0.3'
   url         'https://github.com/tools-aoeur/redmine_stealth'
   author_url  'https://github.com/tools-aoeur'
 
